13fbbfafdd08ea1e5525631fb1b31f466c4f1309
Molla Sazidur Rahman <rsazidur936@gmail.com>
2022-09-11 15:29:58 +0600

    Working with event that quit my game

--- Basic_Movement_.py
+++ Basic_Movement_.py
@@ -4,3 +4,18 @@ pygame.init()
 screen = pygame.display.set_mode((500, 500))
 
 pygame.display.set_caption("First Game")
+
+x = 50
+y = 50
+height = 60
+width = 40
+vel = 5
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+
+pygame.quit()

a72046a5638425b8d45a4127412326912d8bedba
Molla Sazidur Rahman <rsazidur936@gmail.com>
2022-09-11 19:56:50 +0600

    Done Basic_Movement

--- Basic_Movement_.py
+++ Basic_Movement_.py
@@ -9,7 +9,7 @@ x = 50
 y = 50
 height = 60
 width = 40
-vel = 5
+vel = 10
 
 run = True
 while run:
@@ -20,6 +20,18 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        x -= vel
+    if keys[pygame.K_RIGHT]:
+        x += vel
+    if keys[pygame.K_UP]:
+        y -= vel
+    if keys[pygame.K_DOWN]:
+        y += vel
+
+    screen.fill((0, 0, 0))
     # with draw. we can draw something like circle or rect anything it took 3 parameter
     pygame.draw.rect(screen, (255, 0, 0), (x, y, height, width))
     # to see the draw.rect we have to update it, so we can do that by

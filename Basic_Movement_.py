ed94a008a6b390d306d1d3d0c9c99338959156f1
Molla Sazidur Rahman <rsazidur936@gmail.com>
2022-09-11 17:43:16 +0600

    Updated that rect on my display:

--- Basic_Movement_.py
+++ Basic_Movement_.py
@@ -13,9 +13,16 @@ vel = 5
 
 run = True
 while run:
+    pygame.time.delay(100)
+
+    # pygame.event do everything we press and do with our keyboard and mouse
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+    # with draw. we can draw something like circle or rect anything it took 3 parameter
+    pygame.draw.rect(screen, (255, 0, 0), (x, y, height, width))
+    # to see the draw.rect we have to update it, so we can do that by
+    pygame.display.update()
 
 pygame.quit()
